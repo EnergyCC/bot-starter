5a8c194b5b52ed7f62b6c73263845ec71a1a3d8e
EnergyCC <sacasti.vlad@gmail.com>
2017-11-06 07:29:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var owjs = require("overwatch-js");
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg =>{
+
   if (msg.author.id == client.user.id) return;
   if (msg.content === 'ping') {
     msg.reply('Pong');
@@ -69,7 +72,6 @@ if(String(x).startsWith("!num")){
   msg.reply(new Date().format("d-M-Y H:m:S"));
 }*/
 if(msg.content === "!time"){
-  //var timeandDate = require("node.date-time");
   client.channels.get(msg.channel.id).send({
         embed:{
           color: 5423454,
@@ -77,7 +79,47 @@ if(msg.content === "!time"){
         }
   })
   console.log("Command executed successfully: " + msg.content);
-//  msg.reply(new Date().format("d-M-Y H:m:S"));
 }
+//  msg.reply(new Date().format("d-M-Y H:m:S"));
+
+//overwatch here
+
+var overwatchRank = msg.content;
+if(String(overwatchRank).startsWith("!rank")){
+  overwatchRank = overwatchRank.split(" ");
+  owjs.
+        getOverall(overwatchRank[1], overwatchRank[2], overwatchRank[3])
+//        then((data) => client.channels.get(msg.channel.id).send("Nick: " + data.profile.nick + "\n" + "Rank: " + data.profile.rank + "(" + data.profile.ranking + ")"));
+          .then((data) => client.channels.get(msg.channel.id).send({
+                  embed:{
+                    color: 5423454,
+                    description : "Username: " + data.profile.nick +  "\n" + "Rank: " + data.profile.rank + "(" + data.profile.ranking + ")" + "\n" + "Level: " + data.profile.tier + data.profile.level + "\n",
+                    thumbnail: {url: data.profile.rankPicture}
+                  }
+          }))
+          .catch((function(error){
+
+              client.channels.get(msg.channel.id).send("" + error);
+            }
+          )
+        );
+}
+
 console.log(new Date() + " " + msg.author.username + " => Input: " + msg.content);
 });
+
+
+//Grabbing auth data
+
+
+try{
+  var authToken = require("./token.json");
+} catch(e){
+      console.log("A token file does not exist \n" + e.stack);
+      process.exit();
+      }
+if(authToken.token){
+      client.login(authToken.token);
+    }else{
+      console.log("Error logging in. There might be a problem with the token file.")
+    }
